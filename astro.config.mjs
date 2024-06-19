3797bf6fb6a72d245c868df99c02ce7791b49c7b
Samuel <goodlifesam@gmail.com>
2024-06-18 21:20:56 -0700

    netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import node from "@astrojs/node";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'http://localhost:4322/',
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: netlify()
 });
